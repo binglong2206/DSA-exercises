1f7ff5eb933262624ea2362800d709fd78db3c53
jason <binglong2206@gmail.com>
2022-08-17 16:30:52 +0700

    pm done

--- HackerRank/PlusMinus.ts
+++ HackerRank/PlusMinus.ts
@@ -2,12 +2,15 @@ function plusMinus(arr: number[]): void {
     // Write your code here
     const results: [number, number, number] = [0,0,0]
     for (let el of arr) {
-        if (el > 0 ) results[0] = results[0]++
-            else if (el < 0) results[1] = results[1]++
-            else results[2] = results[2]++
+        if (el > 0 ) results[0] += 1
+            else if (el < 0) results[1] +=1
+            else results[2] += 1
     };
     
     for (let el of results){
-        console.log(el/arr.length);
+        console.log((el/arr.length).toFixed(6));
     }
+    // results.forEach(el => {
+    //     console.log((el/arr.length).toFixed(6))
+    // })
 }
