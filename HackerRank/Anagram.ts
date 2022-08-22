ad2a3c86b8022d66c1760fcf2319167b3345dd9f
jason <binglong2206@gmail.com>
2022-08-22 19:33:21 +0700

    anagram done

--- HackerRank/Anagram.ts
+++ HackerRank/Anagram.ts
@@ -11,16 +11,29 @@ interface StringCount {
     [key: string] : number // Index signature, allows any key with number
 }
 
-const rearrange = (str1: string, str2: string): boolean => {
+const checkAnagram = (str1: string, str2: string): boolean => {
+    // Check if length same
+    if (str1.length ! == str2.length) return false
+
     // Initialize the empty objects
     const count1: StringCount = {};
     const count2: StringCount = {};
 
     // Count the strings' characters
+    for (let el of str1) {
+        count1[el] = (count1[el] || 0) + 1
+    }
+    for (let el of str2) {
+        count2[el] = (count2[el] || 0) + 1
+    }
 
-    
-
+    // Verify frequency match
+    for (let key in count1) {
+        if (count1[key] !== count2[key]) return false
+    }
 
 
     return true
 }
+
+console.log(checkAnagram('dog', 'ogd'))
