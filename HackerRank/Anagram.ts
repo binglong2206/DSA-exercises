728a4538f111de7662fb622a1bb86d1b187415fc
jason <binglong2206@gmail.com>
2022-08-22 19:25:57 +0700

    anagram practice with frequency pattern

---
+++ HackerRank/Anagram.ts
@@ -0,0 +1,26 @@
+// Given 2 strings, compare to check if the 2nd string is an anagram to the 1st string
+    /**
+     * Anagram is just a word rearranged to something else, 
+     * Essentially check if both strings have same characters
+     * 
+     * Assume all lower case string, no spaces & special characters
+     * 
+     * */ 
+
+interface StringCount {
+    [key: string] : number // Index signature, allows any key with number
+}
+
+const rearrange = (str1: string, str2: string): boolean => {
+    // Initialize the empty objects
+    const count1: StringCount = {};
+    const count2: StringCount = {};
+
+    // Count the strings' characters
+
+    
+
+
+
+    return true
+}
